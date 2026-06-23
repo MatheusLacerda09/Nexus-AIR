6b136783a961e7da45c07d6c2da358b0ff8dcd93
Matheus Lacerda <matheusulacerda09@gmail.com>
2026-06-23 13:59:51 -0300

    Atualizar o create_database.sql

--- backend/database/create_database.sql
+++ backend/database/create_database.sql
@@ -1,1 +1,10 @@
+CREATE DATABASE nexus_air;
 
+USE nexus_air;
+
+CREATE TABLE usuario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    senha VARCHAR(255) NOT NULL
+);
